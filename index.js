95cdb7f0f7f928b2f0361c0888ef5591a291011e
Shawon Mahmud <shawon.phero@gmail.com>
2023-12-05 04:00:21 +0600

    delete product from the database by using delete  method

--- index.js
+++ index.js
@@ -105,8 +105,16 @@ async function run() {
       res.send(result);
     });
 
+    // delete product by id using delete method
+    app.delete("/delete-product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await storeProductsCollections.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     // console.log(
     //   "Pinged your deployment. You successfully connected to MongoDB!"
     // );
